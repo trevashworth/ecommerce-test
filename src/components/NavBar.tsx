035ebfd2c35128f46b95e0fd00dd5e6c94749511
Trevor Ashworth <trevashworth1@gmail.com>
2025-08-08 17:14:39 -0400

    updated navbar cart link to show items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,26 @@
 import { useAuth } from '../context/AuthContext';
 import { Link } from 'react-router-dom';
 import '../Navbar.css';
+import { useSelector } from 'react-redux';
+import type { RootState } from '../redux/store';
 
 const Navbar = () => {
   const { user } = useAuth();
+  const items = useSelector((state: RootState) => state.cart.items);
+  const totalCount = items.reduce((sum, i) => sum + i.quantity, 0);
+
+
+
   return (
     <div className="nav-container">
       <Link to="/" className="link">Home</Link>
-      <Link to="cart" className="link">Cart</Link>
+      <Link to="cart" className="link cart-link">
+        Cart
+        {totalCount > 0 && <span className="cart-badge">{totalCount}</span>}
+      </Link>
+      
+
+
       {user ? (
         <>
           <Link to="profile" className="link">Profile</Link>
